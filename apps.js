df57618403065654d1e70baea213293b85b9182e
Brent Williams <wtnerb@gmail.com>
2017-11-23 00:48:20 -0800

    cleaned code based on code review; protected global namespace; changed some variable names for clarity

--- apps.js
+++ apps.js
@@ -1,7 +1,6 @@
 //copy/pasted Scott's skeletal structure for the page.
 var allProducts = [];
 var productNames = ['bag', 'banana', 'bathroom', 'boots', 'breakfast', 'bubblegum', 'chair', 'cthulhu', 'dog-duck', 'dragon', 'pen', 'pet-sweep', 'scissors', 'shark', 'tauntaun', 'unicorn', 'usb', 'water-can', 'wine-glass'];
-var indecies = [];
 
 function Product(name, path) {
   // TODO: Build your constructor and necessary properties.
@@ -15,12 +14,11 @@ for (var i = 0; i < productNames.length; i++){
 }
 
 var productRank = {
-  image1: document.getElementById('button1'),
-  image2: document.getElementById('button2'),
-  image3: document.getElementById('button3'),
-  clicked: 0,
-  tot: 0,
+  images: [document.getElementById('button1'), document.getElementById('button2'), document.getElementById('button3')],
+  targt: 0,
+  total: 0,
   myChart: null,
+  indecies: [],//will hold the three indecies of products in allProducts for each iteration
 
   getRandomIndexs: function() {
     //returns array of three random and unique indexes of allProduct array
@@ -28,40 +26,31 @@ var productRank = {
     var num = 0;
     for (var j = 0; j < 3; j++){
       do {
-        num = Math.floor(allProducts.length * Math.random());//looks like off by one error. Isn't.
+        num = Math.floor(allProducts.length * Math.random());
       } while (ind.includes(num));
       ind.push(num);
     }
     return ind;
   },
 
-  displayImages: function(arr) {
-    var b1 = document.getElementById('button1');
-    var b2 = document.getElementById('button2');
-    var b3 = document.getElementById('button3');
-    b1.style.backgroundImage = allProducts[arr[0]].picture;
-    b2.style.backgroundImage = allProducts[arr[1]].picture;
-    b3.style.backgroundImage = allProducts[arr[2]].picture;
+  displayImages: function() {
+    for (i in this.images){
+      this.images[i].style.backgroundImage = allProducts[this.indecies[i]].picture;
+    }
   },
   disableImages: function() {
-    productRank.image1.removeEventListener('click', productRank.onClick);
-    productRank.image2.removeEventListener('click', productRank.onClick);
-    productRank.image3.removeEventListener('click', productRank.onClick);
-    var b1 = document.getElementById('button1');
-    var b2 = document.getElementById('button2');
-    var b3 = document.getElementById('button3');
-    b1.disabled = true;
-    b2.disabled = true;
-    b3.disabled = true;
+    for (i in this.images){
+      this.images[i].disabled = true;
+    }
   },
 
   tallyClicks: function(elementId) {
     if (elementId === 'button1'){
-      allProducts[indecies[0]].votes++;
+      allProducts[this.indecies[0]].votes++;
     } else if (elementId === 'button2'){
-      allProducts[indecies[1]].votes++;
+      allProducts[this.indecies[1]].votes++;
     } else if (elementId === 'button3'){
-      allProducts[indecies[2]].votes++;
+      allProducts[this.indecies[2]].votes++;
     } else console.log('error in tallyClicks method');
   },
 
@@ -105,21 +94,21 @@ var productRank = {
   showButton: function() {
     var button = document.getElementById('display');
     button.style.visibility = 'visible';
-    button.addEventListener('click', productRank.displayResults);
+    button.addEventListener('click', this.displayResults);
   },
 
   onClick: function(event) {
     event.preventDefault();
     locStore.pars();
     console.log(event.target);
-    productRank.clicked = event.target.id;
-    indecies = productRank.getRandomIndexs();
-    productRank.displayImages(indecies);
-    console.log('indecies', indecies);
-    productRank.tallyClicks(productRank.clicked);
-    productRank.tot++;
+    productRank.targt = event.target.id;
+    productRank.indecies = productRank.getRandomIndexs();
+    productRank.displayImages();
+    console.log('indecies', productRank.indecies);
+    productRank.tallyClicks(productRank.targt);
+    productRank.total++;
     locStore.saveData();
-    if (productRank.tot >= 5){//TODO increase after testing
+    if (productRank.total >= 25){//TODO increase after testing
       productRank.disableImages();
       productRank.showButton();
     }
@@ -153,8 +142,9 @@ var locStore = {
 };
 
 var ctx = document.getElementById('myChart').getContext('2d');
-productRank.image1.addEventListener('click', productRank.onClick);
-productRank.image2.addEventListener('click', productRank.onClick);
-productRank.image3.addEventListener('click', productRank.onClick);
-indecies = productRank.getRandomIndexs();
-productRank.displayImages(indecies);
+for (i in productRank.images){
+  productRank.images[i].addEventListener('click', productRank.onClick);
+}
+
+productRank.indecies = productRank.getRandomIndexs();
+productRank.displayImages();
